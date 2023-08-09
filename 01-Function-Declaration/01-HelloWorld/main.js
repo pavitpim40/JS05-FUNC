733753c5f1dcdd6960452fce3ed55cee62061d9c
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-09 14:11:04 +0700

    sol-parameter

---
+++ 01-Function-Declaration/01-HelloWorld/main.js
@@ -0,0 +1,12 @@
+// Define : กำหนด Algorithm + ตั้งชื่อ
+function sayHello() {
+    alert('Hello');
+}
+// call : FN-NAME()
+sayHello();
+sayHello();
+
+// (a) === let a
+function myFunc(a) {
+    console.log(a);
+}
